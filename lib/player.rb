667e9e58e03a9db478431852bcdeb93ecb1ec4c5
Leigh-ann <leigh-ann@andys-MBP.connect>
2017-10-31 21:41:33 +0000

    Player now has feature to keep score - and confirm black_jack

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,13 @@
-
 class Player
 	attr_reader :cards
+	attr_reader :score
+
 	 def initialize(c1,c2)
 	 	@cards = [c1,c2]
+		@score = @cards.inject(0, &:+) 
+	 end
+
+	 def black_jack
+	 	@score == 21
 	 end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,25 @@
 require 'player'
 
 describe Player do 
-	let(:card){double :card}
-	subject{Player.new(:card,:card)}
+	c1 = 10 
+	c2 = 11
 	
+	subject{Player.new(c1,c2)}
+
 	describe 'Initialize' do 
 		it 'Player Initializes with two cards' do 
 			expect(subject.cards.count).to eq 2
 		end
 	end
+
+	describe '#score' do 
+		it 'returns the sum of the card values' do 
+			expect(subject.score).to eq 21
+		end 
+	end 
+
+	describe '#black_jack?'
+		it 'returns true if cards total amount equals 21' do 
+			expect(subject.black_jack).to eq true
+		end
 end 
